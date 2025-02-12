bb2a8aba6ad9ac46f1aaa42ab92f16256ebb3ca7
CK0K <ershovandre06@gmail.com>
2025-02-12 22:43:39 +0000

    Atualização do Codigo Alpha 0.2
    Adição do codigo no main.py e correção do codigo Escola.py

--- classes_v2/Escola.py
+++ classes_v2/Escola.py
@@ -1,3 +1,11 @@
+#######################################
+# Codigo feito por: Gonçalo Feliciano #
+#     Fork feito por: ShovCkOk        #
+#            Data: 02/2025            #
+#######################################
+
+
+"""
 from dataclasses import dataclass
 from modelos import *
 
@@ -40,61 +48,40 @@ class Escola:
 
 
     def listarAlunos(self):
-        return [aluno.nome for aluno in self.alunos]
-    
-    def listarProfessores(self):
-        return [professor.nome for professor in self.professores]
-    
-    def add_aluno(self, aluno):
-        self.alunos.append(aluno)
-    
-    def add_professor(self, professor):
-        self.professores.append(professor)
-    
-    def melhorNota(self, ufcd=None, aluno=None):
-        notas = []
-        for a in self.alunos:
-            if aluno and a != aluno:
-                continue
-            for u, nota in a.notas.items():
-                if ufcd and u != ufcd:
-                    continue
-                notas.append(nota)
-        return max(notas) if notas else None
-    
-    def piorNota(self, ufcd=None, aluno=None):
-        notas = []
-        for a in self.alunos:
-            if aluno and a != aluno:
-                continue
-            for u, nota in a.notas.items():
-                if ufcd and u != ufcd:
-                    continue
-                notas.append(nota)
-        return min(notas) if notas else None
-    
-    def piorMedia(self, ufcd=None, aluno=None, aprovado=True):
-        medias = []
-        for a in self.alunos:
-            if aluno and a != aluno:
-                continue
-            media = sum(a.notas.values()) / len(a.notas) if a.notas else 0
-            if aprovado and media < 10:
-                continue
-            if not aprovado and media >= 10:
-                continue
-            medias.append((a.nome, media))
-        return min(medias, key=lambda x: x[1]) if medias else None
-    
-    def melhorMedia(self, ufcd=None, aluno=None):
-        medias = []
-        for a in self.alunos:
-            if aluno and a != aluno:
-                continue
-            media = sum(a.notas.values()) / len(a.notas) if a.notas else 0
-            medias.append((a.nome, media))
-        return max(medias, key=lambda x: x[1]) if medias else None
+        pass
 
+    def listarProfessores(self):
+        pass
+
+    def add_aluno(self, aluno:Aluno):
+        pass
+
+    def add_professor(self, professor:Professor):
+        pass
+
+    def melhorNota(self, ufcd:str = None, aluno:Aluno = None):
+        pass
+
+    def piorNota(self, ufcd:str = None, aluno:Aluno = None):
+        pass
+
+
+
+    def piorMedia(self, ufcd: str = None, aluno: Aluno = None, aprovado:bool = True):
+    
+        ###################################################
+        #    Lista os alunos com pior media               #
+        #                                                 #
+        #   :param aporvado:                              #
+        #       - True para listar os alunos aprovados,   #
+        #       - False para listar os alunos reprovados, #
+        #                                                 #
+        #   um Aluno aprovado e um aluno com media >= 10  #
+        ###################################################
+        pass
+
+    def melhorMedia(self, ufcd: str = None, aluno: Aluno = None):
+        pass
 
 
     def __int__(self):
@@ -122,3 +109,90 @@ class Escola:
 e = Escola("Escola 1")
 
 e.listarAlunos()
+"""
+
+from modelos import Aluno, Professor, Nota
+
+class Escola:
+
+    def __init__(self, nome: str, lista_alunos=None, lista_professores=None):
+        self.__nome = nome
+        self.lista_alunos = lista_alunos if lista_alunos else []
+        self.lista_professores = lista_professores if lista_professores else []
+        self.__melhorNota = None
+        self.__piorNota = None
+
+    def __update_melhorNota(self, nota:Nota, aluno:Aluno):
+        if self.__melhorNota is None or self.__melhorNota[1].nota < nota.nota:
+            self.__melhorNota = (aluno, nota)
+
+    def __update_piorNota(self, nota: Nota, aluno: Aluno):
+        if self.__piorNota is None or self.__piorNota[1].nota > nota.nota:
+            self.__piorNota = (aluno, nota)
+
+    def __updateTops(self, nota:Nota, aluno:Aluno):
+        self.__update_melhorNota(nota, aluno)
+        self.__update_piorNota(nota, aluno)
+
+    def listarAlunos(self):
+        return self.lista_alunos
+
+    def listarProfessores(self):
+        return self.lista_professores
+    
+    def add_aluno(self, aluno: Aluno):
+        self.lista_alunos.append(aluno)
+
+    def add_professor(self, professor: Professor):
+        self.lista_professores.append(professor)
+    
+    def melhorNota(self, ufcd=None, aluno=None):
+        notas = []
+        for a in self.lista_alunos:
+            if aluno and a != aluno:
+                continue
+            for nota in a.notas:
+                if ufcd and nota.ufcd != ufcd:
+                    continue
+                notas.append(nota)
+        return max(notas, key=lambda x: x.nota) if notas else None
+    
+    def piorNota(self, ufcd=None, aluno=None):
+        notas = []
+        for a in self.lista_alunos:
+            if aluno and a != aluno:
+                continue
+            for nota in a.notas:
+                if ufcd and nota.ufcd != ufcd:
+                    continue
+                notas.append(nota)
+        return min(notas, key=lambda x: x.nota) if notas else None
+
+    def piorMedia(self, ufcd=None, aluno=None, aprovado=True):
+        medias = []
+        for a in self.lista_alunos:
+            if aluno and a != aluno:
+                continue
+            media = a.calcularMedia()
+            if aprovado and media < 10:
+                medias.append((a.nome, media))
+            if not aprovado and media >= 10:
+                medias.append((a.nome, media))
+        return min(medias, key=lambda x: x[1]) if medias else None
+    
+    def melhorMedia(self, ufcd=None, aluno=None):
+        medias = []
+        for a in self.lista_alunos:
+            if aluno and a != aluno:
+                continue
+            media = a.calcularMedia()
+            medias.append((a.nome, media))
+        return max(medias, key=lambda x: x[1]) if medias else None
+
+    def lancarNota(self, nota:float, ufcd:str , aluno: Aluno, professor: Professor):
+        nota_obj = Nota(ufcd, nota, professor)
+        aluno.add_notas(nota_obj)
+        self.__updateTops(nota_obj, aluno)
+
+    def __int__(self):
+        return len(self.lista_alunos)
--- classes_v2/main.py
+++ classes_v2/main.py
@@ -39,10 +39,115 @@ registar professores
 
 
 from Escola import Escola
+from modelos import Aluno, Professor
+
+escola = Escola('Escola Secundária de Vila Nova de Gaia')
+
+def menu():
+    while True:
+        print("\nEscolha uma opção:")
+        print("1 - Adicionar Aluno")
+        print("2 - Adicionar Professor")
+        print("3 - Lançar Nota")
+        print("4 - Listar Alunos")
+        print("5 - Listar Professores")
+        print("6 - Melhor Nota")
+        print("7 - Pior Nota")
+        print("8 - Melhor Média")
+        print("9 - Pior Média")
+        print("10 - Sair")
+        
+        opcao = input("Opção: ")
+        
+        if opcao == "1":
+            nome_aluno = input("Digite o nome do aluno: ")
+            email_aluno = input("Digite o email do aluno: ")
+            turma_aluno = input("Digite a turma do aluno: ")
+            aluno = Aluno(nome=nome_aluno, email=email_aluno, turma=turma_aluno, notas=[])
+            escola.add_aluno(aluno)
+            print(f"Aluno {nome_aluno} adicionado.")
+        
+        elif opcao == "2":
+            nome_professor = input("Digite o nome do professor: ")
+            email_professor = input("Digite o email do professor: ")
+            professor = Professor(nome=nome_professor, email=email_professor)
+            escola.add_professor(professor)
+            print(f"Professor {nome_professor} adicionado.")
+        
+        elif opcao == "3":
+            nome_aluno = input("Digite o nome do aluno: ")
+            aluno = next((a for a in escola.lista_alunos if a.nome == nome_aluno), None)
+            if aluno:
+                nome_professor = input("Digite o nome do professor: ")
+                professor = next((p for p in escola.lista_professores if p.nome == nome_professor), None)
+                if professor:
+                    ufcd = input("Digite a UFCD: ")
+                    try:
+                        nota = float(input("Digite a nota: "))
+                        escola.lancarNota(nota, ufcd, aluno, professor)
+                        print(f"Nota de {nota} lançada para {aluno.nome}.")
+                    except ValueError:
+                        print("Nota inválida!")
+                else:
+                    print("Professor não encontrado.")
+            else:
+                print("Aluno não encontrado.")
+        
+        elif opcao == "4":
+            alunos = escola.listarAlunos()
+            if alunos:
+                print("Lista de Alunos:")
+                for aluno in alunos:
+                    print(f"{aluno.nome} - {aluno.turma} - {aluno.email}")
+            else:
+                print("Nenhum aluno cadastrado.")
+        
+        elif opcao == "5":
+            professores = escola.listarProfessores()
+            if professores:
+                print("Lista de Professores:")
+                for professor in professores:
+                    print(f"{professor.nome} - {professor.email}")
+            else:
+                print("Nenhum professor cadastrado.")
+        
+        elif opcao == "6":
+            melhor_nota = escola.melhorNota()
+            if melhor_nota:
+                print(f"A melhor nota é {melhor_nota.nota} de {melhor_nota.aluno.nome} em {melhor_nota.ufcd}.")
+            else:
+                print("Nenhuma nota registrada.")
+        
+        elif opcao == "7":
+            pior_nota = escola.piorNota()
+            if pior_nota:
+                print(f"A pior nota é {pior_nota.nota} de {pior_nota.aluno.nome} em {pior_nota.ufcd}.")
+            else:
+                print("Nenhuma nota registrada.")
+        
+        elif opcao == "8":
+            melhor_media = escola.melhorMedia()
+            if melhor_media:
+                print(f"A melhor média é de {melhor_media[0]} com média {melhor_media[1]:.2f}.")
+            else:
+                print("Nenhum aluno registrado.")
+        
+        elif opcao == "9":
+            pior_media = escola.piorMedia()
+            if pior_media:
+                print(f"A pior média é de {pior_media[0]} com média {pior_media[1]:.2f}.")
+            else:
+                print("Nenhum aluno registrado.")
+        
+        elif opcao == "10":
+            print("Saindo...")
+            break
+        
+        else:
+            print("Opção inválida!")
+
+# Iniciar o menu
+if __name__ == "__main__":
+    menu()
 
 
-
-es = Escola('Escola Secundaria de Vila Nova de Gaia', [], [])
-
-
-print(es.__dir__())
